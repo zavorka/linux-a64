2962bb693eccc849dc654be6dedd0532f68c3c46
Dimitar Gamishev <gamishev@gmail.com>
2017-05-22 16:34:44 +0300

    sunxi/disp2: Force soft curser for framebuffer console Original patch: https://github.com/armbian/build/blob/6ed9fac60d8c66f91d980f8bd904ff3e55bee13e/patch/kernel/pine64-default/disp2-fb-force-soft-cursor.patch

--- drivers/video/sunxi/disp2/disp/dev_fb.c
+++ drivers/video/sunxi/disp2/disp/dev_fb.c
@@ -654,7 +654,7 @@ static int sunxi_fb_cursor(struct fb_info *info, struct fb_cursor *cursor)
 {
 	__inf("sunxi_fb_cursor\n");
 
-	return 0;
+	return -EINVAL;	/* just to force soft_cursor() call */
 }
 
 s32 drv_disp_vsync_event(u32 sel)
